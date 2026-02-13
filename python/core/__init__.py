53f69a1ae5cb1eb447eab7082e34333c9cd07a16
2p <2pcarrot@gmail.com>
2026-02-13 22:05:51 +0800

    Initialize core module

---
+++ python/core/__init__.py
@@ -0,0 +1,20 @@
+#core/__init__.py
+from .consts import *
+from .frame import build_frame, parse_frame
+from .crypto import (
+    ecdh_generate_key_pair,
+    ecdh_get_public_bytes,
+    ecdh_load_public_bytes,
+    ecdh_derive_aes_key,
+    generate_salt,
+    aes_gcm_encrypt,
+    aes_gcm_decrypt
+)
+from compress import (
+    compress,
+    decompress
+)
+from fec import (
+    fec_encode,
+    fec_decode
+)
